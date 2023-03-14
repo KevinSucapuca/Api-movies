f6686597270d329524d99ab244681e887cca1ac8
KevinSucapuca <ksucapuca007@gmail.com>
2023-03-13 22:59:43 -0500

    add swagger

--- movies/urls.py
+++ movies/urls.py
@@ -1,8 +1,27 @@
 
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title = "Movie API",
+        default_version= "v1",
+        description= "API Con Django",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact= openapi.Contact(email="ksucapuca007@gmail.com"),
+        license= openapi.License(name="MIT License")
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny]
+)
+    
+        
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('movies/', include('movie.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='wagger-docs')
 ]
