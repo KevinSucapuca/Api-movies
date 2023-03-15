2a101f1d43841d6a600d98c621cdcd5ccd20e793
KevinSucapuca <ksucapuca007@gmail.com>
2023-03-14 23:13:17 -0500

    add seguridad a la vista Movies

--- movie/views.py
+++ movie/views.py
@@ -1,7 +1,10 @@
 from .models import Movie
 from .serializer import MovieSerializer
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    #proteger con jwt
+    permission_classes = [IsAuthenticated]
