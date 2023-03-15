c78a329d5d1af9abdda055d4bc00c9d0f1882021
KevinSucapuca <ksucapuca007@gmail.com>
2023-03-14 21:16:59 -0500

    add hashear password views  user

--- user/views.py
+++ user/views.py
@@ -1,7 +1,13 @@
 from django.contrib.auth.models import User
-from .serializers import UserSerializer
+from .serializer import UserSerializer
 from rest_framework import viewsets
+#HASHEAR PASSWORD
+from django.contrib.auth.hashers import make_password
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    #HASHEAR PASSWORD
+    def perform_create(self, serializer):
+        serializer.save(password=make_password(serializer.validated_data['password']))
+        
