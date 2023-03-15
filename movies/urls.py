f413a089043215842ffa825562f39dc00813ebf0
KevinSucapuca <ksucapuca007@gmail.com>
2023-03-14 21:02:46 -0500

    add urls user

--- movies/urls.py
+++ movies/urls.py
@@ -23,6 +23,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('movies/', include('movie.urls')),
+    path('user/', include('user.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='wagger-docs'),
     
 ]
